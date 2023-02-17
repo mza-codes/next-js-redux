815314f368b52123e7b00f2b5b914f1f3da38489
mza-codes <muzofficial90@gmail.com>
2023-02-17 12:36:40 +0000

    Added Redux biolerplate with Hydrate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { wrapper } from "../store";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <main className="bg-gradient-to-br from-cyan-50 via-purple-100 to-indigo-300 min-h-screen w-full flex flex-col items-center justify-center">
+            <Component {...pageProps} />
+        </main>
+    );
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp);
+// export default wrapper.useWrappedStore(MyApp);
